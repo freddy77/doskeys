66e4220660ea3479c75723a8b6224927feba0282
Frediano Ziglio <fziglio@redhat.com>
2019-05-10 16:48:55 +0100

    Fixed indentation

--- kbd.asm
+++ kbd.asm
@@ -7,7 +7,7 @@ org 0x100
 org 0x7c00
 %endif
 
-	xor     ax, ax
+	xor	ax, ax
 
 %ifdef MBR
 	mov	sp, 0x8000
@@ -26,23 +26,23 @@ back_to_zero_segment:
 	mov	cx, (bss_end-bss_start)/2
 	rep	stosw
 
-    mov     es, ax
+	mov	es, ax
 
-    cli                         ; update ISR address w/ ints disabled
-    push    word [es:9*4+2]     ; preserve ISR address
-    push    word [es:9*4]
-    mov     word [es:9*4], irq1isr
-    mov     [es:9*4+2],cs
-    sti
+	cli	                    ; update ISR address w/ ints disabled
+	push	word [es:9*4+2]     ; preserve ISR address
+	push	word [es:9*4]
+	mov	word [es:9*4], irq1isr
+	mov	[es:9*4+2],cs
+	sti
 
-    call    test
+	call	test
 
-    cli                         ; update ISR address w/ ints disabled
-    pop     word [es:9*4]       ; restore ISR address
-    pop     word [es:9*4+2]
-    sti
+	cli	                    ; update ISR address w/ ints disabled
+	pop	word [es:9*4]       ; restore ISR address
+	pop	word [es:9*4+2]
+	sti
 
-    ret
+	ret
 
 ; print al in hexadecimal
 write_hex:
@@ -84,9 +84,9 @@ write_char:
 
 test:
 %ifndef MBR
-	mov     ah, 9
-	mov     dx, msg1
-	int     0x21                ; print "Press ESC to exit"
+	mov	ah, 9
+	mov	dx, msg1
+	int	0x21                ; print "Press ESC to exit"
 %endif
 
 	; print and clear input buffer
@@ -104,9 +104,9 @@ handle_key:
 	cmp	al, 0x1
 	jne	no_esc_key
 
-	mov     ah, 9
-	mov     dx, msg2
-	int     0x21
+	mov	ah, 9
+	mov	dx, msg2
+	int	0x21
 	ret
 no_esc_key:
 %endif
@@ -128,7 +128,7 @@ irq1isr:
 	pusha
 
 	; read keyboard scan code
-	in      al, 0x60
+	in	al, 0x60
 
 	mov	bx, [kbdindex]
 	cmp	byte [bx+kbdcodes], 0
@@ -139,17 +139,17 @@ irq1isr:
 	mov	[kbdindex], bx
 skip_code:
 
-    ; send EOI to XT keyboard
-    in      al, 0x61
-    mov     ah, al
-    or      al, 0x80
-    out     0x61, al
-    mov     al, ah
-    out     0x61, al
+	; send EOI to XT keyboard
+	in	al, 0x61
+	mov	ah, al
+	or	al, 0x80
+	out	0x61, al
+	mov	al, ah
+	out	0x61, al
 
-    ; send EOI to master PIC
-    mov     al, 0x20
-    out     0x20, al
+	; send EOI to master PIC
+	mov	al, 0x20
+	out	0x20, al
 
 	popa
 	pop	es
